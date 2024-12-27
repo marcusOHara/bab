a0d2cebb5bbcbe08bbec209ef7d06d46643cd697
Amishi Mishra <mishmish@unc.edu>
2024-12-27 18:31:34 -0500

    sned

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,27 @@ function App() {
 
   return (
     <>
-      <h1>Babsite</h1>
+      <head>
+          <title>Babsite</title>
+      </head>
+      <body> 
+
+          <br></br>
+          <h1>Babsite</h1>
+          <p>I am a bab. I love my bab</p>
+
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+
+
+          <p> I am sorry cute bab :( I am sorry you have such a bad bab. I love u so much. 
+          </p>
+
+          <img src = 'babshug.png'></img>
+      </body>
     </>
   )
 }
